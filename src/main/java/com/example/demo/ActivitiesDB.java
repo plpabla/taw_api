7fe59bcb76ed0bace726efe4be025bf3bab0bc5f
pawel-laptop <p.blaszczy@gmail.com>
2023-05-29 11:50:56 +0200

    code formatting

--- src/main/java/com/example/demo/ActivitiesDB.java
+++ src/main/java/com/example/demo/ActivitiesDB.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+// @Component definiuje, ze to jest bean (zarzadzalny obiekt w kontenerze Spring)
+// @Scope definiuje cykl zycia - w tym przypadku singleton
 @Component
 @Scope("singleton")
 public class ActivitiesDB {
--- src/main/java/com/example/demo/ActivityDto.java
+++ src/main/java/com/example/demo/ActivityDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.Value;
 
+// @Value dodaje gettery i settery
 @JsonSerialize
 @Value
 public class ActivityDto {
